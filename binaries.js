6f058193ab6ab3bd355de5e0b38efe887365c795
Michael Vlasov <melsomino@gmail.com>
2020-05-06 22:52:00 +0500

    NEW: `binaries_version` in `package.json`

--- binaries.js
+++ binaries.js
@@ -14,14 +14,12 @@
  * limitations under the License.
  */
 
-const fs = require('fs');
+const {version, binaries_version} = require('./package.json');
 const path = require('path');
 const os = require('os');
-
-const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json')));
 const p = os.platform();
 
-const v = pkg.version.split('.');
-const bv = `${v[0]}.${v[1]}.${~~(Number.parseInt(v[2]) / 100) * 100}`.split('.').join('_');
+const v = version.split('.');
+const bv = (binaries_version || `${v[0]}.${v[1]}.${~~(Number.parseInt(v[2]) / 100) * 100}`).split('.').join('_');
 const bp = path.resolve(os.homedir(), '.tonlabs', 'binaries', bv);
 module.exports = { p, bv, bp };
